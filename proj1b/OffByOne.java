d13d99015c46a897776a9a5d4fa146c3d1b17d84
sunhuiquan <sunhuiquan@yeah.net>
2021-01-13 21:21:58 +0800

    add finish task 1, 2, 3

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,3 +1,7 @@
 // A class for off-by-1 comparators.
-public class OffByOne {
+public class OffByOne implements CharacterComparator {
+    @Override
+    public boolean equalChars(char x, char y) {
+        return x == y;
+    }
 }
--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,3 +1,36 @@
 // A class for palindrome operations.
 public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        if (word == null) {
+            return null;
+        }
+        Deque<Character> charDeque = new ArrayDeque<>();
+        int size = word.length();
+        for (int i = 0; i < size; i++) {
+            charDeque.addLast(word.charAt(i));
+        }
+        return charDeque;
+    }
+
+    private boolean isPalindromeRecursive(Deque<Character> charDeque) {
+        if (charDeque.size() <= 1) {
+            return true;
+        }
+        if (!charDeque.removeFirst().equals(charDeque.removeLast())) {
+            return false;
+        }
+        return isPalindromeRecursive(charDeque);
+    }
+
+    public boolean isPalindrome(String word) {
+        if (word == null) {
+            return true;
+        }
+        Deque<Character> charDeque = wordToDeque(word);
+        return isPalindromeRecursive(charDeque);
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        return true;
+    }
 }
